3809a63dd10a5671357e83b95777958dcb9537ed
Remo Vetere <remo.vetere@gmail.com>
2019-01-06 16:02:53 +0100

    finished basic app setup

--- app/@components/layout/layoutView.tsx
+++ app/@components/layout/layoutView.tsx
@@ -1,13 +1,16 @@
+import classNames from 'class-names'
 import { PureComponent, ReactNode } from 'react'
 import { Footer } from './footer'
 import styles from './layoutView.css'
 
 export class LayoutView extends PureComponent {
+  private isDarwin: boolean = process.platform === 'darwin'
+
   public render(): ReactNode {
     const { children } = this.props
     return (
-      <div className={styles.layout}>
-        <div className={styles.dragArea} />
+      <div className={classNames(styles.layout, { [styles.darwin]: this.isDarwin })}>
+        {this.isDarwin && <div className={styles.dragArea} />}
 
         <div className={styles.content}>{children}</div>
 
